9792eeb5694d569b11f128492e805cec9d614c90
ravana09 <pradeep09sathish@gmail.com>
2024-01-04 11:30:50 +0530

    task 2 and 3 done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log("working");
 
-class Movie{
-    constructor(title,studio,rating){
-        this.title=title,
-        this.studio=studio,
-        this.rating=rating||"PG"
+class Movie {
+    constructor(title, studio, rating) {
+        this.title = title,
+            this.studio = studio,
+            this.rating = rating || "PG"
 
     }
-    getPG(){
+    getPG() {
         console.log(`
         The Name of the Movie ${this.title},
         The Movie is produced by ${this.studio},
@@ -18,13 +18,118 @@ class Movie{
 
 }
 
-let CasinoRoyal =new Movie("Casino Royale","Eon Productions","PG­13")
+let CasinoRoyal = new Movie("Casino Royale", "Eon Productions", "PG­13")
 console.log(CasinoRoyal);
 CasinoRoyal.getPG()
 
 //if rating is not given
 
-let newMovie=new Movie("Casino Royale","Eon Productions");
+let newMovie = new Movie("Casino Royale", "Eon Productions");
 console.log(newMovie);
 newMovie.getPG();
+
+//write  a person details
+
+class Person {
+    constructor(firstName, lastName, age, address, education, experience,) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.address = address
+        this.education = education
+        this.experience = experience
+    }
+    getDetails() {
+        console.log(`
+        Hi I am ${this.firstName}${this.lastName},i am a ${this.age} years,
+        i live at ${this.address},
+        i have completed ${this.address}and
+        my work experience is ${this.experience}
+        
+        `)
+    }
+}
+
+let pradeep = new Person("pradeep", "sathish", 22, "salem", "BSC chemistry", "2years experience in Health inspector")
+console.log(pradeep)
+
+pradeep.getDetails();
+
+//uber price 
+
+class UberPrice {
+    constructor(place, kilometer, price) {
+        this.place = place
+        this.kilometer = kilometer
+        this.price = price||10
+        
+
+    }
+    
+    priceDetails() {
+        let bookingCharges=25;
+        if (this.kilometer <= 10) {
+            console.log(`
+            DROP LOCATION ;${this.place},
+            TOTAL COST :${this.kilometer * this.price+bookingCharges}`)
+        } else if (this.kilometer > 10 && this.kilometer <30) {
+            console.log(`
+            DROP LOCATION ;${this.place},
+            TOTAL COST :${this.kilometer * (this.price + 5)+bookingCharges}`)
+        } else{
+            console.log(`
+            DROP LOCATION ;${this.place},
+            TOTAL COST :${this.kilometer * (this.price + 8)+bookingCharges}`)
+        
+        }
+    }
+
+
+}
+
+let orderOne = new UberPrice("salem", 31,10)
+console.log(orderOne.kilometer)
+orderOne.priceDetails()
+
+//CIRCLE
+
+// class Circle{
+//     constructor(radius,color){
+//         this._radius=radius
+//         this._color=color
+
+//     }
+
+//     double(){
+//         this._radius*2
+//     }
+
+//     get radius(){
+//        return this._radius
+//     }
+
+//     get color(){
+//        return this._color
+//     }
+    
+//     set color(colour){
+//         if(colour===""){
+//             this._color=colour==="void"
+//         }else{
+//             this._color=colour
+//         }
+//     }
+
+    
+// }
+
+// let circle =new Circle(1.0,"red")
+// let circle2=new Circle(1.0)
+
+// console.log(circle.radius)
+// console.log(circle.color)
+// console.log(circle2.radius)
+// console.log(circle2.color)
+
+// circle.double()
 
